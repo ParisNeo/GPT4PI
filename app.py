c6942d0741aa23a55ba627c0e20e49785aeac89d
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-04-12 22:36:10 +0200

    upgraded

--- app.py
+++ app.py
@@ -6,17 +6,20 @@
 # What if we bring the power of LLMS to Raspberry PI?
 ######
 from pyllamacpp import model
-def create_model(self):
+def create_model(self, model_path):
     return model.Model(
-        ggml_model=f"./models/{self.args.model}", 
+        ggml_model=model_path, 
         n_ctx=512, 
         )
 def callback(text):
     print(text)
 
 if __name__=="__main__":
-    model = create_model()
+    model_path = f"./models/gpt4all-lora-quantized-ggml.bin"
+    model = create_model(model_path)
     print("Testing GPT4All on Raspberry pi")
     while True:
         prompt = input(">")
         model.generate(prompt,new_text_callback=callback, n_predict=55)
+
+        
